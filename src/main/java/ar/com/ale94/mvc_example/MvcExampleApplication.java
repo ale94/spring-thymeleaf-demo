68070e08a2348bf8927c442263817d040060070d
ale94 <alejandrorua111@gmail.com>
2024-09-22 21:56:36 -0300

    init: first commit

---
+++ src/main/java/ar/com/ale94/mvc_example/MvcExampleApplication.java
@@ -0,0 +1,13 @@
+package ar.com.ale94.mvc_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/com/ale94/mvc_example/MvcExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.ale94.mvc_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
